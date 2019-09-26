3aafcd96ad36132e906f5791c073646c01cd1ba9
Adam Weissman <adam@comedytragedyepic.com>
2019-09-26 08:10:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
   end
   
   def parse
-    @email_addresses.split(",").gsub(/\s+/, "")
+    @email_addresses.split(",").collect {|x| x.gsub(/\s+/, "")}
 =begin
       @email_addresses.split(" ")
     else @email_addresses.include(",")
